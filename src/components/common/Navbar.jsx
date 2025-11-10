b0bdb4cff1150bd4274c6bc9d4315aa9ee293cba
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-11-10 10:26:30 +0600

    navbar designed with logo

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,65 @@
+import { Link, NavLink } from "react-router";
+import "./Navbar.css";
+import Button from "./Button";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li className="mr-2">
+        <NavLink to="/i">Link1</NavLink>
+      </li>
+
+      <li className="mr-2">
+        <NavLink to="/g">Link2</NavLink>
+      </li>
+
+      <li className="mr-2">
+        <NavLink to="/h">Link3</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar fixed container mx-auto bg-[#FFF8E7]/80 backdrop-blur-md border-b border-[#ffffff]/30 rounded-b-xl">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+          {links}
+          </ul>
+        </div>
+        <a className="text-2xl font-bold bg-linear-to-r from-[#FF7B00] via-[#FF3D54] to-[#7CB518] bg-clip-text text-transparent">FlavorHood</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+        {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {/* <a className="btn">Login</a> */}
+        <Button> Login</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../components/common/Navbar';
+import Footer from '../components/common/Footer';
 
 const MainLayout = () => {
     return (
-     <div className="">
+     <div className=" container mx-auto ">
       
       <header>
-        <nav>
-         nav
+        <nav >
+         <Navbar></Navbar>
         </nav>
       </header>
-      <main className="min-h-screen">
+      <main className="min-h-screen  pt-20">
         <Outlet></Outlet>
       </main>
       <footer>
-        footer
+        <Footer/>
       </footer>
     </div>
     );
