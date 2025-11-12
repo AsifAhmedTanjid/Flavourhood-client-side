a023fb5d6bae96437b5458b1914d503fe8062c2f
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-11-12 17:56:08 +0600

    data fetching from mongodb using tanstack for featured and all review page done

--- src/pages/AllReview.jsx
+++ src/pages/AllReview.jsx
@@ -1,65 +1,19 @@
 import React from "react";
 import ReviewCard from "../components/Review/ReviewCard";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../components/common/Loader";
 
 
-const reviews = [
-  {
-    id: 1,
-    foodName: "Margherita Pizza",
-    restaurantName: "Pizza Palace",
-    location: "New York, NY",
-    reviewer: "John Doe",
-    rating: 4.8,
-    photo: "https://source.unsplash.com/400x300/?pizza",
-  },
-  {
-    id: 2,
-    foodName: "Sushi Platter",
-    restaurantName: "Sushi House",
-    location: "Tokyo, Japan",
-    reviewer: "Sakura Tanaka",
-    rating: 4.9,
-    photo: "https://source.unsplash.com/400x300/?sushi",
-  },
-  {
-    id: 3,
-    foodName: "Cheeseburger",
-    restaurantName: "Burger Town",
-    location: "Los Angeles, CA",
-    reviewer: "Mike Smith",
-    rating: 4.7,
-    photo: "https://source.unsplash.com/400x300/?burger",
-  },
-  {
-    id: 4,
-    foodName: "Pasta Carbonara",
-    restaurantName: "Trattoria Roma",
-    location: "Rome, Italy",
-    reviewer: "Luca Bianchi",
-    rating: 4.8,
-    photo: "https://source.unsplash.com/400x300/?pasta",
-  },
-  {
-    id: 5,
-    foodName: "Chocolate Cake",
-    restaurantName: "Sweet Tooth",
-    location: "Paris, France",
-    reviewer: "Marie Dubois",
-    rating: 4.9,
-    photo: "https://source.unsplash.com/400x300/?cake",
-  },
-  {
-    id: 6,
-    foodName: "Tacos",
-    restaurantName: "El Mexicano",
-    location: "Mexico City, Mexico",
-    reviewer: "Carlos Ramirez",
-    rating: 4.7,
-    photo: "https://source.unsplash.com/400x300/?tacos",
-  },
-];
 
 const AllReview = () => {
+    const {data,isPending} =useQuery({
+        queryKey:['allreviews'],
+        queryFn: getAllReviews
+    })
+
+    console.log(data);
+    
+
   return (
     <section className="py-16 bg-[#FFFDF5]">
       <div className="container mx-auto">
@@ -67,15 +21,22 @@ const AllReview = () => {
           Flavorful Reviews
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {reviews.map((review) => (
-            <ReviewCard review={review} key={review.id}></ReviewCard>
+       {
+        isPending? <Loader></Loader>: <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {data.map((review) => (
+            <ReviewCard review={review} key={review._id}></ReviewCard>
           ))}
         </div>
+       }
       </div>
     </section>
   );
 };
+
+const getAllReviews=async()=>{
+    const response= await fetch("http://localhost:3000/reviews")
+    return await response.json()
+}
 
 export default AllReview;
 
